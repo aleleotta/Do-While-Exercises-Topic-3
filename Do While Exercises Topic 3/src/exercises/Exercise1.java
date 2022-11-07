f82544cce0d4c063c7beb99c298e7f12bbee9a3f
aleleotta <aleleotta@106-02.info2>
2022-11-07 09:13:43 +0100

    Exercise 1 completed and documented.

---
+++ Do While Exercises Topic 3/src/exercises/Exercise1.java
@@ -0,0 +1,15 @@
+package exercises;
+import java.util.*;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		int i = 0; //Declared variable
+		do { //Do this first before checking.
+			System.out.println("Current number: " + i); //Print
+			i++; //i variable goes up by 1.
+		} 
+		while(i <= 20); //Set limit
+	}
+}
+/*Realiza un programa que muestre por pantalla los 20 
+primeros números naturales (1, 2, 3... 20).*/
